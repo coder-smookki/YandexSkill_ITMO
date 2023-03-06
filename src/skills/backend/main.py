9992dbefc3491573a0ae89668586eac332b5f658
Кирилл <medved3loy@yandex.ru>
2023-03-06 19:29:29 +0500

    The basis of the skill

--- src/skills/backend/main.py
+++ src/skills/backend/main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 sessionStorage = {}
 
 
+# Главная функция которая запускает обработчик диалогового окна
 @app.route('/post', methods=['POST'])
 def main():
     response = \
@@ -16,11 +17,12 @@ def main():
                 'end_session': False
             }
         }
-    general_menu(request.json, response)
+    handle_dialog(request.json, response)
     return json.dumps(response, ensure_ascii=False)
 
 
-def general_menu(req, res):
+# Функция обработки диалогового окна
+def handle_dialog(req, res):
     user_id = req['session']['user_id']
     if req['session']['new']:
         sessionStorage[user_id] = \
@@ -41,9 +43,43 @@ Hello Student! To use the skill, choose a language.
         1. Русский Язык (Russian Language)
         2. Английский Язык (English Language)
             """
-        res['response']['buttons'] = sessionStorage[user_id]
+        res['response']['buttons'] = [
+            {
+                'title': button,
+                'hide': True
+            } for button in sessionStorage[user_id]['suggests']
+        ]
+        res['response']['card'] = {
+            "type": "BigImage",
+            "image_id": '',
+            "title": '',
+            "description": ''
+        }
         return
+    else:
+        tokens = req['request']['nlu']['tokens']
+        russian_language_intent = {'Русский Язык', 'Русский', 'Русс', 'Рус', 'Ру', 'Русск', 'Рсскй', 'Рсский', 'Русскй',
+                                   'Russian Language', 'Russian', 'Russ', 'Rus', 'Ru', 'Russin', 'Rssn', 'Russn',
+                                   'русский язык', 'Русский язык', 'русский Язык' 'русский', 'русс', 'рус', 'ру',
+                                   'русск', 'рсскй', 'рсский', 'русскй', 'Russian language', 'russian Language',
+                                   'russian', 'russ', 'rus', 'ru', 'russin', 'rssn', 'russn'}
+        english_language_intent = {'English language', 'english language', 'English Language', 'english Language',
+                                   'Английский Язык', 'английский язык', 'Английский язык', 'английский Язык',
+                                   'Английский', 'английский', 'англ', 'Англ', 'Англский', 'англский', 'Англск',
+                                   'англск', 'English', 'english', 'En', 'Englsh', 'Engish', 'en', 'englsh', 'engish',
+                                   'Eng', 'eng', }
+        exit_skills_intent = {'Выйти', 'выйти', 'вйти', 'Вйти', 'выходи', 'exit', 'Exit', 'Ext', 'ext', 'выход'}
+        help_skills_intent = {'Помощь', 'помощь', 'пмщь', 'Пмщь', 'Help', 'help'}
+        if list(set(tokens) & russian_language_intent):
+            pass
+        if list(set(tokens) & english_language_intent):
+            pass
+        if list(set(tokens) & exit_skills_intent):
+            pass
+        if list(set(tokens) & help_skills_intent):
+            pass
 
 
+# Запуск программы
 if __name__ == '__main__':
     app.run()
