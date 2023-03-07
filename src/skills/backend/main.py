a07103a45847a4641a49b1d5e11d114fe9e92c50
Кирилл <medved3loy@yandex.ru>
2023-03-07 13:34:50 +0500

    Working basis

--- src/skills/backend/main.py
+++ src/skills/backend/main.py
@@ -1,85 +1,94 @@
-from flask import Flask, request
-import json
-
-app = Flask(__name__)
-
-sessionStorage = {}
+from src.skills.backend.backend import *
 
 
-# Главная функция которая запускает обработчик диалогового окна
-@app.route('/post', methods=['POST'])
-def main():
-    response = \
-        {
-            'session': request.json['session'],
-            'version': request.json['version'],
-            'response': {
-                'end_session': False
-            }
-        }
-    handle_dialog(request.json, response)
-    return json.dumps(response, ensure_ascii=False)
+# def handler(event, context):
+#    """
+#    Точка входа для облачной функции.
+#    :param event: содержимое request.json().
+#    :param context: информация о текущем контексте выполнения.
+#    :return: ответ будет представлен в виде json автоматически.
+#    """
+#    text = 'Привет, я повторю все, что вы скажете'
+#    if 'request' in event and \
+#            'original_utterance' in event['request'] \
+#           and len(event['request']['original_utterance']) > 0:
+#        text = event['request']['original_utterance']
+#    return {
+#        'version': event['version'],
+#        'session': event['session'],
+#        'response': {
+#            'text': text,
+#            'end_session': 'false'
+#        },
+#    }
+
+def main(event, context):
+    """
+    Точка входа для облачной функции.
+    :param event: содержимое request.json().
+    :param context: информация о текущем контексте выполнения.
+    :return: Ответ будет представлен в виде json автоматически.
+    """
+    if event['session']['new'] == 'true':
+        return general_menu(event, context)
+    if event['session']['new'] == 'false':
+        pass
 
 
-# Функция обработки диалогового окна
-def handle_dialog(req, res):
-    user_id = req['session']['user_id']
-    if req['session']['new']:
-        sessionStorage[user_id] = \
-            {
-                'suggests': [
-                    'Русский Язык',
-                    'Английский язык',
-                    'Выйти',
-                    'Помощь'
-                ]
-            }
-        res['response']['text'] = \
-            """
+def general_menu(event, context):
+    """
+    Точка входа для облачной функции.
+    :param event: содержимое request.json().
+    :param context: информация о текущем контексте выполнения.
+    :return: Ответ будет представлен в виде json автоматически.
+    """
+    buttons = {
+        'suggests': [
+            'Русский Язык (Russian Language)',
+            'Английский язык (English Language)',
+            'Выйти (Exit)',
+            'Помощь (Help)'
+        ]
+    }
+    return {
+        'version': event['version'],
+        'session': event['session'],
+        'response': {
+            'text':
+                """
 Привет Студент! Чтобы пользоваться навыком, выбери язык.
 
 Hello Student! To use the skill, choose a language.
 
-        1. Русский Язык (Russian Language)
-        2. Английский Язык (English Language)
-            """
-        res['response']['buttons'] = [
-            {
-                'title': button,
-                'hide': True
-            } for button in sessionStorage[user_id]['suggests']
-        ]
-        res['response']['card'] = {
-            "type": "BigImage",
-            "image_id": '',
-            "title": '',
-            "description": ''
+       1. Русский Язык (Russian Language)
+       2. Английский Язык (English Language)
+                """,
+            'tts':
+                """
+Привет Студент! Чтобы пользоваться навыком, выбери язык.
+Hello Student! To use the skill, choose a language.
+       Первое - это Русский Язык (Russian Language)
+       Второе - это Английский Язык (English Language)
+                """,
+            'card': {
+                'type': 'BigImage',
+                'image_id': '937455/40f0536e426907808499',
+                'title': 'Главное меню (General menu)',
+                'description':
+                    """
+Привет Студент! Чтобы пользоваться навыком, выбери язык.
+Hello Student! To use the skill, choose a language.
+1 - Русский Язык (Russian Language).
+2 - Английский Язык (English Language).
+                """,
+            },
+            'buttons': [
+                {
+                    'title': title,
+                    'payload': {},
+                    'hide': 'true'
+                } for title in buttons['suggests']
+            ],
+            'end': 'false'
         }
-        return
-    else:
-        tokens = req['request']['nlu']['tokens']
-        russian_language_intent = {'Русский Язык', 'Русский', 'Русс', 'Рус', 'Ру', 'Русск', 'Рсскй', 'Рсский', 'Русскй',
-                                   'Russian Language', 'Russian', 'Russ', 'Rus', 'Ru', 'Russin', 'Rssn', 'Russn',
-                                   'русский язык', 'Русский язык', 'русский Язык' 'русский', 'русс', 'рус', 'ру',
-                                   'русск', 'рсскй', 'рсский', 'русскй', 'Russian language', 'russian Language',
-                                   'russian', 'russ', 'rus', 'ru', 'russin', 'rssn', 'russn'}
-        english_language_intent = {'English language', 'english language', 'English Language', 'english Language',
-                                   'Английский Язык', 'английский язык', 'Английский язык', 'английский Язык',
-                                   'Английский', 'английский', 'англ', 'Англ', 'Англский', 'англский', 'Англск',
-                                   'англск', 'English', 'english', 'En', 'Englsh', 'Engish', 'en', 'englsh', 'engish',
-                                   'Eng', 'eng', }
-        exit_skills_intent = {'Выйти', 'выйти', 'вйти', 'Вйти', 'выходи', 'exit', 'Exit', 'Ext', 'ext', 'выход'}
-        help_skills_intent = {'Помощь', 'помощь', 'пмщь', 'Пмщь', 'Help', 'help'}
-        if list(set(tokens) & russian_language_intent):
-            pass
-        if list(set(tokens) & english_language_intent):
-            pass
-        if list(set(tokens) & exit_skills_intent):
-            pass
-        if list(set(tokens) & help_skills_intent):
-            pass
-
-
-# Запуск программы
-if __name__ == '__main__':
-    app.run()
+    }
