d297b71c033c6f3da711349db1c90ab266c3823e
$ H V E P S <102893182+coder-medved@users.noreply.github.com>
2023-03-11 13:39:25 +0500

    Update connect.py

--- src/skills/backend/connect.py
+++ src/skills/backend/connect.py
@@ -3,12 +3,12 @@ import MySQLdb
 
 def connect():
     conn = MySQLdb.connect(
-        host="rc1b-7ap37y1reqrnu47t.mdb.yandexcloud.net",
-        port=3306,
-        db="utmo",
-        user="mytable",
-        passwd="utmoyandexadmin",
-        ssl={'ca': '~/.mysql/root.crt'})
+        host=HOST,
+        port=PORT,
+        db=DBNAME,
+        user=USERNAME,
+        passwd=PASSWORDNAME,
+        ssl=SSL)
 
     cur = conn.cursor()
     cur.execute('SELECT version()')
