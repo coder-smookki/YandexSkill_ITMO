db480f655b63feed3771f3f36461a8a684e23fcd
elogrus <elogrus@gmail.com>
2023-03-14 23:49:58 +0500

    fix exitConfirm

--- src/backend/dialogs/exitConfirm/exitConfirm.py
+++ src/backend/dialogs/exitConfirm/exitConfirm.py
@@ -5,15 +5,22 @@ from utils.branchHandler import getDialogResponseFromEnd
 import copy
 
 config = getConfig()
+
+
 def getResponse(event, allDialogs=None):
     if isInContext(event, 'exitConfirm') and isSimilarCommand(event, 'да'):
-        response = {\
-            'response': {'end_session': True},
-            'session_state': config['session_state'],
+        newConfig = {'response': {
+            'text': config['message'],
+            'tts': config['tts'],
+            'card': config['card'] if 'card' in config else {},
+            'buttons': createButtons(config['buttons']),
+            'end_session': config['end_session'] if 'end_session' in config else False
+        },
             'session': event['session'],
-            'version': event['version']
-        }
-        return response
+            'session_state': config['session_state'] if 'session_state' in config else {'branch': ''},
+            'version': event['version']}
+        newConfig['end_session'] = True
+        return createResponse(event, newConfig)
     elif isInContext(event, 'exitConfirm'):
         return getDialogResponseFromEnd(event, 2, allDialogs)
     return createResponse(event, config)
@@ -22,5 +29,5 @@ def getResponse(event, allDialogs=None):
 def isTriggered(event):
     return isSimilarCommand(event, 'выйти') or isInContext(event, 'exitConfirm') and isSimilarCommand(event, 'да')
 
- 
+
 exitConfirm = {'getResponse': getResponse, 'isTriggered': isTriggered}
