6c7477a36227b8b1d429ecb27d09dcd3d77c41d9
elogrus <elogrus@gmail.com>
2023-03-14 14:55:00 +0500

    commit

--- post.py
+++ post.py
@@ -1,8 +1,23 @@
 import requests
 from datetime import datetime
+import threading
 
-startTime = datetime.now()
-res = requests.post('https://ssdteam.ru', {'hehehe': 'hehehehehehe'})
-endTime = datetime.now()
-print('Time: ' + str(endTime - startTime))
-print(res)
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def asd():
+    startTime = datetime.now()
+    res = requests.post('https://ssdteam.ru:2083', {'hehehe': 'hehehehehehe'})
+    endTime = datetime.now()
+    print('Time: ' + str(endTime - startTime))
+    print(res)
+
+set_interval(asd, 0.5)
+
+
+
