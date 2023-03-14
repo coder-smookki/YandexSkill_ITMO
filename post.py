d6c7ab73b46320da281a01d39545d627bf4e6d70
elogrus <elogrus@gmail.com>
2023-03-14 14:50:48 +0500

    commit

--- post.py
+++ post.py
@@ -1,3 +1,8 @@
 import requests
+from datetime import datetime
 
-requests.post('')
+startTime = datetime.now()
+res = requests.post('https://ssdteam.ru', {'hehehe': 'hehehehehehe'})
+endTime = datetime.now()
+print('Time: ' + str(endTime - startTime))
+print(res)
