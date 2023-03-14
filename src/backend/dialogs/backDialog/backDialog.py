a20f50f6f52c3e57095c1333ce7e6f27fba80208
elogrus <elogrus@gmail.com>
2023-03-14 21:17:12 +0500

    fix branches

--- src/backend/dialogs/backDialog/backDialog.py
+++ src/backend/dialogs/backDialog/backDialog.py
@@ -2,7 +2,7 @@ from utils.triggerHelper import *
 from utils.branchHandler import *
 
 def getResponse(event, allDialogs=None):
-    return getDialogResponseFromEnd(event, 1, allDialogs)
+    return getDialogResponseFromEnd(event, 2, allDialogs)
 
 def isTriggered(event):
     return isSimilarCommand(event, 'назад')
