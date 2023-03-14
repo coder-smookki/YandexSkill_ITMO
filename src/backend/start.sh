d52af91bc0c1efd694607749f0bc90f0227b9a83
elogrus <elogrus@gmail.com>
2023-03-14 21:57:54 +0500

    loop to sh script

--- src/backend/start.sh
+++ src/backend/start.sh
@@ -1,7 +1,7 @@
 while true
 do
-	echo "Press [CTRL+C] to stop.."
-	sleep 1
-    git pull
-    gunicorn --bind 0.0.0.0:2083 index:app
+	echo "Press [CTRL+C] to stop..";
+	sleep 1;
+    git pull;
+    gunicorn --bind 0.0.0.0:2083 index:app;
 done
