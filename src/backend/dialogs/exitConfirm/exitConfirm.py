e4230c36e2571e6c502eef6d02e5bda5db8b1d94
elogrus <elogrus@gmail.com>
2023-03-14 23:38:17 +0500

    fix exitConfirm

--- src/backend/dialogs/exitConfirm/exitConfirm.py
+++ src/backend/dialogs/exitConfirm/exitConfirm.py
@@ -5,16 +5,23 @@ from utils.branchHandler import getDialogResponseFromEnd
 import copy
 
 config = getConfig()
+
+
 def getResponse(event, allDialogs=None):
     if isInContext(event, 'exitConfirm') and isSimilarCommand(event, 'да'):
-        newConfig = copy.deepcopy(config)
-        newConfig['end_session'] = True
-        return createResponse(event, newConfig)
+        response = {\
+            'response': {'end_session': True},
+            'session': event['session'],
+            'version': event['version']
+        }
+        return response
     elif isInContext(event, 'exitConfirm'):
         return getDialogResponseFromEnd(event, 2, allDialogs)
     return createResponse(event, config)
 
+
 def isTriggered(event):
     return isSimilarCommand(event, 'выйти') or isInContext(event, 'exitConfirm') and isSimilarCommand(event, 'да')
+
 
 exitConfirm = {'getResponse': getResponse, 'isTriggered': isTriggered}
