211ec49ee644887bf4a6bed1c0e8ad19e77718a7
Кирилл <medved3loy@yandex.ru>
2023-03-15 20:37:34 +0500

    add block - to a foreign student

---
+++ src/backend/dialogs/toAForeignStudent/toAForeignStudent.py
@@ -0,0 +1,15 @@
+from .config import getConfig
+from utils.responseHelper import *
+from utils.triggerHelper import *
+
+config = getConfig()
+def getResponse(event, allDialogs=None):
+    return createResponse(event, config)
+
+
+def isTriggered(event):
+    token = {"иностранный", "иностранному", "иностранцу"}
+    return isSimilarTokens(event, token) and isInContext(event, 'russianMenu')
+
+
+studentOffice = {'getResponse': getResponse, 'isTriggered': isTriggered}
