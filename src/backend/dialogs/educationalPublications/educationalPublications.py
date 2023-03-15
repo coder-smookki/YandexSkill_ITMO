45a5260f50fdac8a645584d5b421eb3651587515
elogrus <elogrus@gmail.com>
2023-03-16 00:45:33 +0500

    + eduPublic

--- src/backend/dialogs/educationalPublications/educationalPublications.py
+++ src/backend/dialogs/educationalPublications/educationalPublications.py
@@ -6,29 +6,26 @@ import copy
 
 origConfig = getConfig()
 def getResponse(event, allDialogs=None):
-    sessionId = getSessionId(event)
-    if sessionId in globalStorage and globalStorage[sessionId] != False:
-        info = globalStorage['sessionId']
-        config = copy.deepcopy(origConfig)
-        for elem in info:
-            config['buttons'].insert(0, {
-                'title': str(info.index(elem)),
-                'url': elem['link']
-            })
+    info = globalStorage['sessionId']
+    config = copy.deepcopy(origConfig)
+    for elem in info:
+        config['buttons'].insert(0, {
+            'title': str(info.index(elem)),
+            'url': elem['link']
+        })
 
-            config['message'] += f"""
+        config['message'] += f"""
                 {str(info.index(elem)) + '. ' + elem['title']}.\n
                 {elem['text']}.\n
                 ------------\n
             """
-        return createResponse(event, config)
-    if sessionId in globalStorage:
-        return createLoadingResponse(event, 'Еще загружаем...')
-    return createLoadingResponse(event, 'Загрузка...')
+    return createResponse(event, config)
+
 
 def isTriggered(event):
     token = {"учебные", "издания", "учебны"}
     return isSimilarTokens(event, token) and isInContext(event, 'russianMenu')
 
 
-educationalPublications = {'getResponse': getResponse, 'isTriggered': isTriggered}
+educationalPublications = {
+    'getResponse': getResponse, 'isTriggered': isTriggered}
