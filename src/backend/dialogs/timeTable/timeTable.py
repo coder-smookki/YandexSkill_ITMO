48511186a625dc63a15113eed01f8000c4469a7c
elogrus <elogrus@gmail.com>
2023-03-16 23:35:15 +0500

    fix timetable

--- src/backend/dialogs/timeTable/timeTable.py
+++ src/backend/dialogs/timeTable/timeTable.py
@@ -2,29 +2,50 @@ from .config import getConfig
 from utils.responseHelper import *
 from utils.triggerHelper import *
 
+
 def getResponse(event, allDialogs=None):
-    if not haveState(event, 'timeTable_group'):
-        print('1')
-        config = copy.deepcopy(getConfig('group'))
-        config['session_state']['timeTable_group'] = 'notEntered'
+    if not haveState(event, "timeTable_step"):
+        print("1")
+        config = copy.deepcopy(getConfig("group"))
+        config["session_state"]["timeTable_step"] = 1
         return createResponse(event, config)
-    
-    elif not haveState(event, 'timeTable_course'):
-        print('2')
-        config = copy.deepcopy(getConfig('course'))
-        config['session_state']['timeTable_course'] = 'notEntered'
-        config['session_state']['timeTable_group'] = getOriginalUtterance(event)
+
+    elif getState(event, "timeTable_step") == 1:
+        print("2")
+        config = copy.deepcopy(getConfig("course"))
+        config["session_state"]["timeTable_step"] = 2
+        config["session_state"]["timeTable_group"] = getOriginalUtterance(event)
         return createResponse(event, config)
-    
-    setStateInEvent(event, 'timeTable_group', getState(event, 'timeTable_group'))
-    setStateInEvent(event, 'timeTable_course', getOriginalUtterance(event))
-    return allDialogs['timeTableFind']['getResponse'](event, allDialogs)
+
+    elif getState(event, "timeTable_step") == 2:
+        print("2")
+        config = copy.deepcopy(getConfig("degree"))
+        config["session_state"]["timeTable_step"] = 3
+        config["session_state"]["timeTable_group"] = getState(event, "timeTable_group")
+        config["session_state"]["timeTable_course"] = getOriginalUtterance(event)
+        return createResponse(event, config)
+    elif getState(event, "timeTable_step") == 3:
+        print("2")
+        config = copy.deepcopy(getConfig("degree"))
+        config["session_state"]["timeTable_step"] = 3
+        config["session_state"]["timeTable_group"] = getState(event, "timeTable_group")
+        config["session_state"]["timeTable_course"] = getState(event, "timeTable_course")
+        config["session_state"]["timeTable_degree"] = getOriginalUtterance(event)
+        return createResponse(event, config)
+
+    setStateInEvent(event, "timeTable_group", getState(event, "timeTable_group"))
+    setStateInEvent(event, "timeTable_course", getOriginalUtterance(event))
+    return allDialogs["timeTableFind"]["getResponse"](event, allDialogs)
 
 
 def isTriggered(event):
     token = {"занятий", "расписание", "расписание"}
 
-    return isSimilarTokens(event, token) and isInContext(event, 'russianMenu') or isInContext(event, 'timeTable')
+    return (
+        isSimilarTokens(event, token)
+        and isInContext(event, "russianMenu")
+        or isInContext(event, "timeTable")
+    )
 
 
-timeTable = {'getResponse': getResponse, 'isTriggered': isTriggered}
+timeTable = {"getResponse": getResponse, "isTriggered": isTriggered}
--- src/backend/dialogs/timeTableFind/config.py
+++ src/backend/dialogs/timeTableFind/config.py
@@ -9,6 +9,17 @@ session_state = {"branch": "timeTable"}
 def getConfig(event):
     group = getState(event, "timeTable_group")
     course = int(getState(event, "timeTable_course"))
+    degree = int(getState(event, 'timeTable_degree'))
+
+    if 'бакал' in degree or 'unde' in degree:
+        degree = 3
+    elif 'магис' in degree or 'magis' in degree: 
+        degree = 4
+    elif 'спец' in degree or 'spec' in degree: 
+        degree = 4
+    else:
+        raise ValueError("Сould not recognize degree: " + degree)
+
     message = ""
     tts = ""
 
