ee8db3bf52c2c52df8b854ab5e7522b7c891e0b5
Ivan_Karavaev <koertyf@gmail.com>
2023-03-17 17:37:34 +0400

    quis update

--- src/backend/dialogs/general_quiz/config.py
+++ src/backend/dialogs/general_quiz/config.py
@@ -22,7 +22,7 @@ def getConfig(num, event):
     correct_answer = questions["answers"][num]
     answers = questions["uncorrect_answers"]
     print(answers)
-    answers[random.randint(0, len(answers))] = correct_answer
+    answers[random.randint(0, len(answers) - 1)] = correct_answer
     buttons_response = answers + buttons
     states = {
         "count_questions": event["state"]["session"]["count_questions"],
