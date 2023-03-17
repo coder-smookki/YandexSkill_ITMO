5c750e1aa83e0898c51e63ac34cd0342a20c5a6d
elogrus <elogrus@gmail.com>
2023-03-17 18:51:09 +0500

    fix timetable

--- src/backend/dialogs/timeTableFind/config.py
+++ src/backend/dialogs/timeTableFind/config.py
@@ -1,21 +1,15 @@
+from utils.responseHelper import *
 from utils.parser.parser import *
-from utils.responseHelper import getState
 
-def getConfig(event):
-    group = getState(event, "timeTable_group")
-    degree = getState(event, 'timeTable_degree')
-
+def getConfig(event, pageNum):
     message = ""
     tts = ""
-    buttons = ["Помощь", "Назад", "Выйти"]
 
-    announces = parser("timetable.getGroupTimetable", [group, degree])
-    if not announces:
-        message = 'Произошла какая-то ошибка. Скорее всего, вы ввели недействительные данные.'
-        tts = 'Произошла какая-то ошибка. Скорее всего, вы ввели недействительные данные.'
-        buttons.insert(0, 'Попробовать еще раз')
-    else:
-        for i in announces:
+    if not (pageNum is None) and pageNum >= 1:
+        countOnOnePage = 3
+        startPagesArrFrom = (pageNum - 1) * countOnOnePage
+        pages = getState(event, 'timeTable_timetable')
+        for i in pages[pageNum:startPagesArrFrom + countOnOnePage]:
             message += f"""
             {i['dayWeek']}
             {i['date']}
@@ -29,8 +23,37 @@ def getConfig(event):
             {i['classFormat']}
             ------------
             """
+        session_state = {
+            'branch': 'timeTable',
+            'timeTable_timetable': timetable
+        }
+        return {
+            "message": message,
+            "tts": tts,
+            "buttons": buttons,
+            'session_state': session_state
+        }
+
+    group = getState(event, "timeTable_group")
+    degree = getState(event, 'timeTable_degree')
+    timetable = parser("timetable.getGroupTimetable", [group, degree])
+    buttons = ["Помощь", "Назад", "Выйти"]
+
+    session_state = {
+        'branch': 'timeTable',
+        'timeTable_timetable': timetable
+    }
+    setStateInEvent(event, 'timeTable_timetable', timetable)
+
+    if not timetable:
+        message = 'Произошла какая-то ошибка. Скорее всего, вы ввели недействительные данные.'
+        tts = 'Произошла какая-то ошибка. Скорее всего, вы ввели недействительные данные.'
+        buttons.insert(0, 'Попробовать еще раз')
+    else:
+        getConfig(event, 1)
     return {
         "message": message,
         "tts": tts,
-        "buttons": buttons
+        "buttons": buttons,
+        'session_state': session_state
     }
--- src/backend/dialogs/timeTableFind/timeTableFind.py
+++ src/backend/dialogs/timeTableFind/timeTableFind.py
@@ -2,13 +2,17 @@ from .config import getConfig
 from utils.responseHelper import *
 from utils.triggerHelper import *
 
-
 def getResponse(event, allDialogs=None):
+    if isSimilarCommand('Следующая страница'):
+        pass
+    if isSimilarCommand('Предыдущая страница'):
+        pass
+
     config = getConfig(event)
     return createResponse(event, config)
     
 def isTriggered(event):
-    return False
+    return isInContext(event, 'timeTable') and (isSimilarCommand('Следующая страница') or isSimilarCommand('Предыдущая страница'))
 
 
 timeTableFind = {'getResponse': getResponse, 'isTriggered': isTriggered}
