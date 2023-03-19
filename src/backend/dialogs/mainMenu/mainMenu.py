3ff99187b2fd722ab6d5a515a3c5f9a766fb82ec
elogrus <elogrus@gmail.com>
2023-03-19 10:49:48 +0500

    add language

--- src/backend/dialogs/mainMenu/mainMenu.py
+++ src/backend/dialogs/mainMenu/mainMenu.py
@@ -5,13 +5,11 @@ from utils.triggerHelper import *
 def getResponse(event, allDialogs=None):
     config = getConfig(event)
     if isInLastContext('chooseLanguage'):
-        if 'русский' in getCommand(event) or 'рус' in getCommand(event) or 'ру' in getCommand(event)
+        if 'русский' in getCommand(event) or 'рус' in getCommand(event) or 'ру' in getCommand(event):
             setGlobalStateInEvent(event, 'language', 'ru-RU')
         else:
             setGlobalStateInEvent(event, 'language', 'en-US')
-    a = createResponse(event, config)
-    # print(a)
-    return a
+    return createResponse(event, config)
 
 
 def isTriggered(event):
