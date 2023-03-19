89a0f4675af16e98a0b66d8019b2a42f4b4b7da0
Ivan_Karavaev <koertyf@gmail.com>
2023-03-19 15:58:17 +0400

    update buttons in announce

--- src/backend/dialogs/announces/announces.py
+++ src/backend/dialogs/announces/announces.py
@@ -8,16 +8,20 @@ def getResponse(event, allDialogs=None):
     config = getConfig()
     announces = globalStorage['news_announces']
     buttonsResponse = []
-    for i in announces:
-        config['message'] += f"""
-        {i['text']}.\n
-        {i['date']}.\n
-        ------------\n
-        """
-        buttonsResponse.append({'title': i['text'], 'url': i['link'], 'hide': False})
-        config['tts'] += f'Вы направились в категорию "Анонсы". {i["text"]} будет {i["date"]} '
+    if len(announces) == 0:
+        config['message'] = "Новый новостей на данный момент нет."
+        config['tts'] = "Новый новостей на данный момент нет"
+    else:
+        for i in announces:
+            config['message'] += f"""
+            {i['text']}.\n
+            {i['date']}.\n
+            ------------\n
+            """
+            buttonsResponse.append({'title': i['text'], 'url': i['link'], 'hide': False})
+            config['tts'] += f'Вы направились в категорию "Анонсы". {i["text"]} будет {i["date"]} '
 
-    config['buttons'] = buttonsResponse + config['buttons']
+        config['buttons'] = buttonsResponse + config['buttons']
     return createResponse(event, config)
 
 
