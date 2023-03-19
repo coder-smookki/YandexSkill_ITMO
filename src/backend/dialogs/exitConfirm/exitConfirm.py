7f1bbbd27bd932a48a747319e52963ec6bb9c64b
elogrus <elogrus@gmail.com>
2023-03-19 19:05:44 +0500

    fix dialogs

--- src/backend/dialogs/exitConfirm/exitConfirm.py
+++ src/backend/dialogs/exitConfirm/exitConfirm.py
@@ -8,7 +8,15 @@ config = getConfig()
 
 def getResponse(event, allDialogs=None):
     print("exitConfirm")
-    if isInContext(event, "exitConfirm") and isSimilarCommand(event, "да"):
+    if isInContext(event, "exitConfirm") and (
+        "да" in getCommand(event)
+        or "конечно" in getCommand(event)
+        or "уверен" in getCommand(event)
+        or "точно" in getCommand(event)
+        or "выйти" in getCommand(event)
+        or "выход" in getCommand(event)
+        or "выйди" in getCommand(event)
+    ):
         response = {
             "response": {
                 "text": "До скорых встреч! Были рады вас видеть в нашем навыке!",
