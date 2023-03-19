3ac6d6b5ff7ca4cc97629942e5281ee880380419
elogrus <elogrus@gmail.com>
2023-03-19 17:02:28 +0500

    fix dialogs

--- src/backend/dialogs/announces/announces.py
+++ src/backend/dialogs/announces/announces.py
@@ -8,15 +8,9 @@ def getResponse(event, allDialogs=None):
     config = getConfig()
     announces = globalStorage['news_announces']
     buttonsResponse = []
-<<<<<<< HEAD
     if len(announces) <= 0:
         config['message'] += 'К сожалению, анонсов сейчас нет =('
         config['tts'] += 'К сожалению, анонсов сейчас нет'
-=======
-    if len(announces) == 0:
-        config['message'] = "Новый новостей на данный момент нет."
-        config['tts'] = "Новый новостей на данный момент нет"
->>>>>>> 89a0f4675af16e98a0b66d8019b2a42f4b4b7da0
     else:
         for i in announces:
             config['message'] += f"""
@@ -24,11 +18,7 @@ def getResponse(event, allDialogs=None):
             {i['date']}.\n
             ------------\n
             """
-<<<<<<< HEAD
             buttonsResponse.append({'title': i['text'], 'url': i['link']})
-=======
-            buttonsResponse.append({'title': i['text'], 'url': i['link'], 'hide': False})
->>>>>>> 89a0f4675af16e98a0b66d8019b2a42f4b4b7da0
             config['tts'] += f'Вы направились в категорию "Анонсы". {i["text"]} будет {i["date"]} '
 
         config['buttons'] = buttonsResponse + config['buttons']
