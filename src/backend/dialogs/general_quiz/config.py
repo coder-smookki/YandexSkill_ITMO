d5969e01da4d4b73dfeb523f6bb05b8cdb07f763
Ivan_Karavaev <koertyf@gmail.com>
2023-03-19 13:44:37 +0400

    update quiz

--- src/backend/dialogs/general_quiz/config.py
+++ src/backend/dialogs/general_quiz/config.py
@@ -22,7 +22,7 @@ def getConfig(event):
     message = questions["questions"][getState(event, "questions_list")[-1]]
     correct_answer = questions["answers"][getState(event, "questions_list")[-1]]
     answers = copy.deepcopy(questions["uncorrect_answers"])
-    answers[random.randint(0, len(answers))] = correct_answer
+    answers[random.randint(0, len(answers) - 1)] = correct_answer
     buttons_response = answers + buttons
     states = {
         "count_questions": event["state"]["session"]["count_questions"],
