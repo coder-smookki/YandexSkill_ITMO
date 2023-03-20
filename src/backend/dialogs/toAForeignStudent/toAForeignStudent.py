3fa5a328b9577c2547f3292b059b8f4f7c74031c
elogrus <elogrus@gmail.com>
2023-03-20 10:18:29 +0500

    fix dialogs

--- src/backend/dialogs/toAForeignStudent/toAForeignStudent.py
+++ src/backend/dialogs/toAForeignStudent/toAForeignStudent.py
@@ -1,17 +1,39 @@
-from .config import getConfig
+from .config import *
 from utils.responseHelper import *
 from utils.triggerHelper import *
-
-config = getConfig()
+from utils.branchHandler import getDialogResponseFromEnd
+mainConfig = getMainConfig()
+bechelorConfig = getBechelorConfig()
+magistracyConfig = getMagistracyConfig()
+internationalMagistracyConfig = getInternationalMagistracyConfig()
+additionalOpportsConfig = getAdditionalOpportsConfig()
 
 
 def getResponse(event, allDialogs=None):
-    return createResponse(event, config)
+    if not isInLastContext('toAForeignStudent'):
+        return createResponse(event, mainConfig)
+    
+    if 'бакал' in getCommand(event):
+        return createResponse(event, bechelorConfig)
+
+    if ('между' in getCommand(event) or 'народ' in getCommand(event)) and 'магис' in getCommand(event):
+        return createResponse(event, internationalMagistracyConfig)
+
+    if 'магис' in getCommand(event):
+        return createResponse(event, magistracyConfig)
+
+    if 'докум' in getCommand(event):
+        return createResponse(event, mainConfig)
+
+    if 'возм' in getCommand(event) or 'доп' in getCommand(event):
+        return createResponse(event, additionalOpportsConfig)
+
+    return getDialogResponseFromEnd(event, 1, allDialogs)
 
 
 def isTriggered(event):
     token = {"иностранный", "иностранному", "иностранцу"}
-    return isSimilarTokens(event, token) and isInContext(event, 'mainMenu')
+    return isInLastContext('toAForeignStudent') or isSimilarTokens(event, token) and isInContext(event, 'mainMenu')
 
 
 toAForeignStudent = {'getResponse': getResponse, 'isTriggered': isTriggered}
