274f0724f1803e26748cde4d17cb2a59654decda
elogrus <elogrus@gmail.com>
2023-03-20 17:59:16 +0500

    fix dialogs

--- src/backend/dialogs/chooseLanguage/chooseLanguage.py
+++ src/backend/dialogs/chooseLanguage/chooseLanguage.py
@@ -10,8 +10,21 @@ def getResponse(event, allDialogs=None):
 
 
 def isTriggered(event):
-    changeLangTokens = {'язык', 'language', 'lang'}
-    return isInLastContext(event, 'mainMenu') and isSimilarTokens(event, changeLangTokens)
+    return (
+        "ченж" in getCommand(event)
+        or "чендж" in getCommand(event)
+        or "ченж" in getCommand(event)
+        or "чанж" in getCommand(event)
+        or "чандж" in getCommand(event)
+        or "chan" in getCommand(event)
+        or "chen" in getCommand(event)
+    ) and (
+        "ланг" in getCommand(event)
+        or "лонг" in getCommand(event)
+        or "lang" in getCommand(event)
+        or "язык" in getCommand(event)
+        or "езык" in getCommand(event)
+    )
 
 
-chooseLanguage = {'getResponse': getResponse, 'isTriggered': isTriggered}
+chooseLanguage = {"getResponse": getResponse, "isTriggered": isTriggered}
