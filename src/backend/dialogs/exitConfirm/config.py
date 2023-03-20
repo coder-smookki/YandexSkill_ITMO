f7a2f06135a83f090d8b9aa060c8b53538207813
elogrus <elogrus@gmail.com>
2023-03-20 18:16:09 +0500

    fix dialogs

--- src/backend/dialogs/exitConfirm/config.py
+++ src/backend/dialogs/exitConfirm/config.py
@@ -1,37 +1,51 @@
-message = \
-    """
-    Вы уверены в том, что хотите выйти?
-    """
+from utils.responseHelper import getLanguage
 
-tts = \
-    """
-    Вы уверены в том, что хотите выйти из навыка?
-    """
-
-buttons = [
-    "Да",
-    "Нет",
-]
-
-card = {
-    'type': 'BigImage',
-    'image_id': '937455/40f0536e426907808499',
-    'title': 'ВЫЙТИ ИЗ НАВЫКА?',
-    'description': \
-        """
-        Вы уверены в том, что хотите выйти?
-        """
+config = {
+    "ru-RU": {
+        "tts": """
+            Вы уверены в том, что хотите выйти?
+            """,
+        "buttons": [
+            "Да",
+            "Нет",
+        ],
+        "card": {
+            "type": "BigImage",
+            "image_id": "937455/40f0536e426907808499",
+            "title": "ВЫЙТИ ИЗ НАВЫКА?",
+            "description": """
+                Вы уверены в том, что хотите выйти?
+                """,
+        },
+    },
+    "en-US": {
+        "tts": """
+            Are you sure you want to exit?
+            """,
+        "buttons": [
+            "Yes",
+            "No",
+        ],
+        "card": {
+            "type": "BigImage",
+            "image_id": "937455/40f0536e426907808499",
+            "title": "EXIT SKILL?",
+            "description": """
+                Are you sure you want to exit?
+                """,
+        },
+    },
 }
 
-session_state = {
-    "branch": "exitConfirm"
-}
+session_state = {"branch": "exitConfirm"}
 
 
-def getConfig():
+def getConfig(event):
+    lang = getLanguage(event)
+    
     return {
-        'message': message,
-        'tts': tts,
-        'buttons': buttons,
-        'session_state': session_state
+        "tts": config[lang]['tts'],
+        "buttons": config[lang]['buttons'],
+        "card": config[lang]['card'],
+        "session_state": config[lang]['session_state'],
     }
